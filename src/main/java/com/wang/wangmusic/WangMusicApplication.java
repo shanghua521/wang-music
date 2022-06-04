c0a05e123d4207a053b61d232d40862727787547
cZXvmXNqWs <2621197736@qq.com>
2022-06-04 20:45:37 +0800

    init

---
+++ src/main/java/com/wang/wangmusic/WangMusicApplication.java
@@ -0,0 +1,13 @@
+package com.wang.wangmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WangMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WangMusicApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/wang/wangmusic/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.wang.wangmusic.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "v1/hello")
+public class HelloController {
+
+    @RequestMapping
+    public String hello() {
+        return "hello";
+    }
+
+}
